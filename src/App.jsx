e12606c07a355cd26923bc5ecf2bbd1cb207ffc8
Ivan López Rodríguez <ivaan100@gmail.com>
2025-06-11 13:07:33 +0200

    add: useDebounce search

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 import { useState, useEffect } from 'react'
+import { useDebounce } from 'react-use'
 import Search from './components/Search'
-import Spinner from './components/Spinner'
 import MovieCard from './components/MovieCard'
+import Spinner from './components/Spinner'
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
@@ -19,13 +20,19 @@ function App () {
   const [errorMessage, setErrorMessage] = useState('')
   const [movieList, setMovieList] = useState([])
   const [isLoading, setIsLoading] = useState(false)
+  const [debouncedSearchTerm, setDebouncedSearchTerm] = useState('')
 
-  const fetchMovies = async () => {
+  // Debounce search term to prevent making too many API Requests
+  useDebounce(() => setDebouncedSearchTerm(searchTerm), 500, [searchTerm])
+
+  const fetchMovies = async (query = '') => {
     setIsLoading(true)
     setErrorMessage('')
 
     try {
-      const endpoint = `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
+      const endpoint = query
+        ? `${API_BASE_URL}/search/movie?query=${encodeURIComponent(query)}`
+        : `${API_BASE_URL}/discover/movie?sort_by=popularity.desc`
       const response = await fetch(endpoint, API_OPTIONS)
 
       if (!response.ok) {
@@ -50,8 +57,8 @@ function App () {
   }
 
   useEffect(() => {
-    fetchMovies()
-  }, [])
+    fetchMovies(debouncedSearchTerm)
+  }, [debouncedSearchTerm])
 
   return (
     <main>
@@ -64,7 +71,7 @@ function App () {
         </header>
 
         <section className='all-movies'>
-          <h2>All Movies</h2>
+          <h2 className='mt-[40px]'>All Movies</h2>
 
           {isLoading
             ? (
