5ec8a4c811422461ae73a1082800399039e66bb5
Ivan López Rodríguez <ivaan100@gmail.com>
2025-06-11 16:01:44 +0200

    add: UpdateSearchCount function

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import { useDebounce } from 'react-use'
 import Search from './components/Search'
 import MovieCard from './components/MovieCard'
 import Spinner from './components/Spinner'
+import { updateSearchCount } from './appwrite'
 
 const API_BASE_URL = 'https://api.themoviedb.org/3'
 const API_KEY = import.meta.env.VITE_TMDB_API_KEY
@@ -44,12 +45,16 @@ function App () {
       if (data.Response === 'False') {
         setErrorMessage(data.Error || 'Failed to fetch movies')
         setMovieList([])
+        return
       }
 
       setMovieList(data.results || [])
+
+      if (query && data.results.length > 0) {
+        await updateSearchCount(query, data.results[0])
+      }
     } catch (error) {
       console.error(`Error fetching movies: ${error}`)
-
       setErrorMessage('Error fetching movies. Please try again later.')
     } finally {
       setIsLoading(false)
